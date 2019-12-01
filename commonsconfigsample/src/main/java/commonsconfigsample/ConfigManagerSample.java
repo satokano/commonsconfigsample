ea2dc67842aa5756bbdbd8bf631291e358f7bc17
Satoshi Okano <satokano@gmail.com>
2019-12-01 15:38:05 +0900

    とりあえずpropertiesファイルから値がとれるところまで

--- commonsconfigsample/src/main/java/commonsconfigsample/ConfigManagerSample.java
+++ commonsconfigsample/src/main/java/commonsconfigsample/ConfigManagerSample.java
@@ -1,7 +1,28 @@
 package commonsconfigsample;
 
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.FileBasedConfiguration;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+
 public class ConfigManagerSample {
-    public boolean someLibraryMethod() {
-        return true;
+    
+    public String getString(String key) {
+        Parameters params = new Parameters();
+        FileBasedConfigurationBuilder<FileBasedConfiguration> builder = 
+                new FileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration.class)
+                .configure(params.properties().setFileName("sample.properties"));
+        
+        try {
+            Configuration config = builder.getConfiguration();
+            String value = config.getString(key);
+            return value;
+        } catch (ConfigurationException cex) {
+            throw new RuntimeException(cex);
+        }
+        
     }
+    
 }
--- commonsconfigsample/src/test/java/commonsconfigsample/ConfigManagerSampleTest.java
+++ commonsconfigsample/src/test/java/commonsconfigsample/ConfigManagerSampleTest.java
@@ -1,13 +1,13 @@
 package commonsconfigsample;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ConfigManagerSampleTest {
+
     @Test
-    public void testSomeLibraryMethod() {
-        ConfigManagerSample classUnderTest = new ConfigManagerSample();
-        fail("未実装ですよ");
-    }
+        public void testGetString() throws Exception {
+            ConfigManagerSample underTest = new ConfigManagerSample();
+            Assertions.assertThat(underTest.getString("sample1")).isEqualTo("value1");
+        }
 }
