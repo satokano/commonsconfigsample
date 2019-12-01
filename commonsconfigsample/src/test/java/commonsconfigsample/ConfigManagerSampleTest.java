1f6c6036cd98dd2f34b5d5c2bf4beb86fd1ea204
Satoshi Okano <satokano@gmail.com>
2019-12-02 01:02:50 +0900

    includeを使ってみる

--- commonsconfigsample/src/test/java/commonsconfigsample/ConfigManagerSampleTest.java
+++ commonsconfigsample/src/test/java/commonsconfigsample/ConfigManagerSampleTest.java
@@ -6,8 +6,16 @@ import org.junit.jupiter.api.Test;
 public class ConfigManagerSampleTest {
 
     @Test
-        public void testGetString() throws Exception {
-            ConfigManagerSample underTest = new ConfigManagerSample();
-            Assertions.assertThat(underTest.getString("sample1")).isEqualTo("value1");
-        }
+    public void testGetString() throws Exception {
+        ConfigManagerSample underTest = new ConfigManagerSample();
+        Assertions.assertThat(underTest.getString("sample1")).isEqualTo("value1");
+    }
+    
+    @Test
+    public void testGetString_option1() throws Exception {
+        ConfigManagerSample underTest = new ConfigManagerSample();
+        
+        Assertions.assertThat(underTest.getString("subsystem.title")).isEqualTo("commonsconfiguration2sample_option1");
+        
+    }
 }
