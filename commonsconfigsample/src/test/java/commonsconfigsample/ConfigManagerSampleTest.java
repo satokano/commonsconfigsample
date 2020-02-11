01d2a635f69a2b6ecff49c4db5e973c5056b16c1
Satoshi OKANO <okano@ENVY2020>
2020-02-11 19:09:11 +0900

    環境変数に依存する設定項目のパターンを追加

--- commonsconfigsample/src/test/java/commonsconfigsample/ConfigManagerSampleTest.java
+++ commonsconfigsample/src/test/java/commonsconfigsample/ConfigManagerSampleTest.java
@@ -14,8 +14,13 @@ public class ConfigManagerSampleTest {
     @Test
     public void testGetString_option1() throws Exception {
         ConfigManagerSample underTest = new ConfigManagerSample();
-        
         Assertions.assertThat(underTest.getString("subsystem.title")).isEqualTo("commonsconfiguration2sample_option1");
-        
+    }
+    
+    @Test
+    public void testGetEnvValue1() throws Exception {
+    	ConfigManagerSample underTest = new ConfigManagerSample();
+    	// モックライブラリ使うと環境変数もセットできるらしいが。ひとまず普段はEclipseで設定しておくようにする。
+    	Assertions.assertThat(underTest.getString("env1")).isEqualTo("hogehogevalue");
     }
 }
